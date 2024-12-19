0ed882ace62282db673c2e9cf3434a263f36081e
apclccruz <lccruz@student.apc.edu.ph>
2024-12-20 00:18:34 +0800

    final

--- home/js/myScript.js
+++ home/js/myScript.js
@@ -1,3 +1,7 @@
+function myFunction() {
+    document.getElementById("demo").innerHTML="Paragraph changed.";
+}
+
 function myFunction2() {
     const element = document.getElementsByTagName("p");
  
@@ -5,9 +9,3 @@ function myFunction2() {
     document.getElementById("demo").innerHTML = '<br> The text in first paragraph (index 0) is: <br>' + element[0].innerHTML;
  
 }
- 
-index.html
-<script
-location.reload();
-src="js/myScript.js">
-</script>
