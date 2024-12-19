f4e01284558179c3a705718306c32e65094e001b
apclccruz <lccruz@student.apc.edu.ph>
2024-12-20 00:06:42 +0800

    final

--- home/js/myScript.js
+++ home/js/myScript.js
@@ -6,3 +6,4 @@ function myFunction2() {
     const element = document.getElementsByTagName("h1");
  
 document.getElementById("demo").innerHTML = 'The text in first h1 tag (index 0) is: ' + element[0].innerHTML;
+}
