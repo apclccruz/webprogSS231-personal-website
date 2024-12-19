582de0d656d649d78d82457e2a4891b19be42c9c
apclccruz <lccruz@student.apc.edu.ph>
2024-12-19 23:55:07 +0800

    changes

--- home/js/myScript.js
+++ home/js/myScript.js
@@ -1,3 +1,8 @@
 function myFunction() {
     document.getElementById("demo").innerHTML="Paragraph changed.";
 }
+
+function myFunction2() {
+    const element = document.getElementsByTagName("h1");
+ 
+document.getElementById("demo").innerHTML = 'The text in first h1 tag (index 0) is: ' + element[0].innerHTML;
